300ee61462f14245b9ae2ba55126454128de3ab3
aaaddress1 <aaaddress1@gmail.com>
2016-03-24 14:26:58 +0800

    no message

---
+++ checker/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(191, 341);
+            this.listBox1.TabIndex = 0;
+
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(191, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(400, 341);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 341);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Windows Process Module Digital Signature Verifier, by aaaddress1.";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ checker/Form1.cs
@@ -0,0 +1,85 @@
+﻿using Security.WinTrust;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows.Forms;
+
+namespace checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("wintrust.dll", PreserveSig = true, SetLastError = false)]
+        public static extern uint WinVerifyTrust(IntPtr hWnd, IntPtr pgActionID, IntPtr pWinTrustData);
+
+        Process[] pArray;
+        bool isHang = true;
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Form.CheckForIllegalCrossThreadCalls = false;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(delegate(object o, EventArgs a)
+            {
+                try
+                {
+                    Process curr = pArray[this.listBox1.SelectedIndex];
+                    richTextBox1.Text = "Target Process = " + curr.ProcessName + " , pid = " + curr.Id + "\n";
+                    foreach (ProcessModule i in curr.Modules)
+                    {
+                        richTextBox1.Text += i.FileVersionInfo.ToString() + "path: " + i.FileName + "\n";
+
+                        WinVerifyTrustResult result = WinTrust.VerifySignatureResult(i.FileName);
+                        richTextBox1.Text += ("簽章確認結果:\t"+result + "\n");
+                        richTextBox1.Text += "==============================\n";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    richTextBox1.Text = ex.ToString();
+                }
+            });
+            this.listBox1.GotFocus += new System.EventHandler(delegate(Object o, EventArgs a)
+            {
+                isHang = true;
+            });
+            this.listBox1.LostFocus += new System.EventHandler(delegate(Object o, EventArgs a)
+            {
+                isHang = false;
+            });
+            (new System.Threading.Thread(() =>
+            {
+                while (true)
+                {
+                    if (isHang) continue;
+                    if (pArray == null ||Process.GetProcesses().Length != pArray.Length)
+                    {
+                        pArray = Process.GetProcesses();
+                        this.Invoke(new MethodInvoker(() => listBox1.Items.Clear()));
+                        foreach (var i in pArray)
+                            this.Invoke(new MethodInvoker(() => listBox1.Items.Add(i.Id + "\t" + i.ProcessName)));
+                    }
+                    else
+                    {
+                        System.Threading.Thread.Sleep(1000);
+                        continue;
+                    }
+    
+                }
+            }) { IsBackground = true }).Start();
+            richTextBox1.Focus();
+        }
+
+     
+
+
+    }
+}
